c6e86528ab21d4cb8ded2238f0bde508faa5b38d
Pabitra Dhara <pabitradharardh@gmail.com>
2023-09-03 20:29:11 +0530

    signup form

--- examportal/examfront/src/app/app-routing.module.ts
+++ examportal/examfront/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { SignupComponent } from './pages/signup/signup.component';
+const routes: Routes = [
+  {
+  path:'signup',
+  component:SignupComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
