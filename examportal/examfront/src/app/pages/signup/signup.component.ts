f8c8f6115a0e3483aabd08c46f56e33f360d82b4
Pabitra Dhara <pabitradharardh@gmail.com>
2023-09-07 22:59:20 +0530

    form connect to user server

--- examportal/examfront/src/app/pages/signup/signup.component.ts
+++ examportal/examfront/src/app/pages/signup/signup.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit{
-
+  constructor(private userService:UserService){}
   public user={
     username:'',
     password:'',
@@ -18,6 +18,23 @@ export class SignupComponent implements OnInit{
 ngOnInit():void{}
 
 formSubmit(){
-  console.log(this.user)
+  console.log(this.user);
+  if(this.user.username=='' || this.user.username==null){
+    alert('User is required');
+    return;
+  }
+
+  //addUser
+  this.userService.addUser(this.user).subscribe(
+    (date)=>{
+      //success
+      alert('success');
+    },
+    (error)=>{
+      //error
+      console.log(error);
+      alert('somthing went to wrong');
+    }
+  )
 }
 }
