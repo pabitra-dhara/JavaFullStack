f5d59c8b96a53e475b7b7f0b83e99ce87d47817b
Pabitra Dhara <pabitradharardh@gmail.com>
2023-09-08 23:25:58 +0530

    from to user service connection done

--- examportal/examserver/src/main/java/com/exam/contollers/UserContollers.java
+++ examportal/examserver/src/main/java/com/exam/contollers/UserContollers.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.exam.services.UserServices;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserContollers {
 
 	@Autowired
