3c49097d439131e7b2bfa26144ba95b1bfbbae96
Pabitra Dhara <pabitradharardh@gmail.com>
2023-09-21 20:38:54 +0530

    security configaration

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -3,12 +3,13 @@ package com.exam.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-import com.exam.services.impl.UserSecurityServiceImpl;
-
+@SuppressWarnings("deprecation")
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MySecurityConfig extends UserSecurityServiceImpl{
-
+public class MySecurityConfig extends WebSecurityConfigurerAd
+{
+	
 }
