1a0109d214467d48fe249f4884dbfa2114af614f
Pabitra Dhara <pabitradharardh@gmail.com>
2023-09-28 21:53:08 +0530

    security

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -1,17 +1,29 @@
 package com.exam.config;
 
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+import jakarta.servlet.Filter;
 
 
 
 @EnableWebSecurity
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class MySecurityConfig{
+public class MySecurityConfig extends WebSecurityConfiguration{
 
+	@Override
+	public Filter springSecurityFilterChain() throws Exception {
+		// TODO Auto-generated method stub
+		return super.springSecurityFilterChain();
+	}
+
+
+	
+	
 }
