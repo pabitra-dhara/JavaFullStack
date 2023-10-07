24b78ba98101658adcf29bd006b712b9154a8b49
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-07 19:47:42 +0530

    update

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -2,6 +2,7 @@ package com.exam.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,10 +13,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class MySecurityConfig{
 	
+	
+	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-		return null;
-		
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http.csrf().disable()
+		.authorizeHttpRequests((authorize)->authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+	
+		return http.build();
 	}
 
 }
