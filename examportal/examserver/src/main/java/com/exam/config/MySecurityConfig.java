26cfa6b4c891be3e729efd7cb8dfaa482a72e30f
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-08 23:44:05 +0530

    updata

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -12,6 +15,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @Configuration
 public class MySecurityConfig{
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails normalUser=User.withUsername("pabitra").password("password").roles("ROLE_NORMAL").build();
+		
+		
+		UserDetails adminUser=User.withUsername("pabitra1").password("password").roles("ROLE_ADMIN").build();
+		return null;
+	}
 	
 	
 	
