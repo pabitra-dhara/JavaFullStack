8b8aa46546c2a5a7270f290dbdd4c3b2a9d2dfa4
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-09 20:14:48 +0530

    error

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -1,38 +1,29 @@
 package com.exam.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 
-@EnableWebSecurity
+@EnableWebMvc
 @Configuration
 public class MySecurityConfig{
 	
 	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails normalUser=User.withUsername("pabitra").password("password").roles("ROLE_NORMAL").build();
-		
-		
-		UserDetails adminUser=User.withUsername("pabitra1").password("password").roles("ROLE_ADMIN").build();
-		return null;
-	}
-	
-	
-	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-		http.csrf().disable()
-		.authorizeHttpRequests((authorize)->authorize.anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
-	
-		return http.build();
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+		httpSecurity.
+		csrf().disable()
+		.authorizeHttpRequests()
+		return httpSecurity.build();
 	}
 
 }
