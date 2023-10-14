caec354155cbb9274e9374ff1685c4053c60a935
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-14 21:30:25 +0530

    fix

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -21,5 +25,18 @@ public class MySecurityConfig{
 				).httpBasic(Customizer.withDefaults());
 		return http.build();
 	}
-
+	public UserDetailsService userDetailsService() {
+		UserDetails pabitra=User.builder()
+				.username("pabitra")
+				.password("pabitra")
+				.roles("USER")
+				.build();
+		
+		UserDetails admin=User.builder()
+				.username("admin")
+				.password("admin")
+				.roles("ADMIN")
+				.build();
+		return new InMemoryUserDetailsManager(pabitra,admin);
+	}
 }
