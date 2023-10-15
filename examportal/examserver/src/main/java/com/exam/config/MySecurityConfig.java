3135ae6eda30eac237c6c3c66b3d7243e7e94cb6
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-15 23:28:52 +0530

    error

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -17,6 +19,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Configuration
 public class MySecurityConfig{
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -25,16 +31,17 @@ public class MySecurityConfig{
 				).httpBasic(Customizer.withDefaults());
 		return http.build();
 	}
+	@Bean
 	public UserDetailsService userDetailsService() {
 		UserDetails pabitra=User.builder()
 				.username("pabitra")
-				.password("pabitra")
+				.password(passwordEncoder().encode("pabitra")
 				.roles("USER")
 				.build();
 		
 		UserDetails admin=User.builder()
 				.username("admin")
-				.password("admin")
+				.password(passwordEncoder().encode("admin")
 				.roles("ADMIN")
 				.build();
 		return new InMemoryUserDetailsManager(pabitra,admin);
