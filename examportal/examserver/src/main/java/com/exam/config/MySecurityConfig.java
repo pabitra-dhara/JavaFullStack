1d39a337cb950b757b2b53f7f778b86fc8a5ebdf
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-17 23:47:10 +0530

    fix

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -1,49 +1,32 @@
 package com.exam.config;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import com.exam.services.impl.UserDetailsServiceImpl;
 
-
-@EnableWebMvc
 @Configuration
+@EnableWebSecurity
 public class MySecurityConfig{
-	@Bean
-	public static PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	@Autowired
+	private UserDetailsServiceImpl userDetailsServiceImpl;
 	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-		http.csrf().disable()
-		.authorizeHttpRequests((authorize)->authorize.anyRequest().authenticated()
-				).httpBasic(Customizer.withDefaults());
-		return http.build();
+	 	public static BCryptPasswordEncoder passwordEncoder() {
+	 		return new BCryptPasswordEncoder();
+	 	}
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.userDetailsService(this.userDetailsServiceImpl).passwordEncoder(passwordEncoder());
 	}
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails pabitra=User.builder()
-				.username("pabitra")
-				.password(passwordEncoder().encode("pabitra")
-				.roles("USER")
-				.build();
+	 
+	protected void configure(HttpSecurity http) throws Exception{
 		
-		UserDetails admin=User.builder()
-				.username("admin")
-				.password(passwordEncoder().encode("admin")
-				.roles("ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(pabitra,admin);
 	}
+
 }
