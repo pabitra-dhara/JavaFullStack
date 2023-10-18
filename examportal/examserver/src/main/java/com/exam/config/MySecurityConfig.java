50fc09d313fb440ef7527214bb60790f0db8514d
Pabitra Dhara <pabitradharardh@gmail.com>
2023-10-18 22:13:35 +0530

    add

--- examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
+++ examportal/examserver/src/main/java/com/exam/config/MySecurityConfig.java
@@ -3,10 +3,13 @@ package com.exam.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.exam.services.impl.UserDetailsServiceImpl;
 
@@ -26,7 +29,19 @@ public class MySecurityConfig{
 	}
 	 
 	protected void configure(HttpSecurity http) throws Exception{
+		http
+		.csrf().disable()
+		.cors().disable()
+		.authorizeRequests()
+		.antMatchers("/generate-token","/user/").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.exceptionHandling().authenticationEntryPoint(unauthorizesHandler)
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		
+		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 
 }
